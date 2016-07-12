623ad69822d3a4469eba0680ba44c0d4cc6d4fcf
Matthew Gardner <mgardner@kenzanmedia.com>
2016-07-12 16:57:02 -0400

    Fixed linting problems

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,8 @@
 var del = require('del');
 var gulp = require('gulp');
 var minifyCssPipeline = require('./src/index.js');
-var testPipeline = require('pipeline-test-node')();
-var validatePipeline = require('pipeline-validate-js')();
-
+var testPipeline = require('pipeline-test-node');
+var validatePipeline = require('pipeline-validate-js');
 var config = {
   cssFiles: [
     'test/**/*.css'
@@ -31,7 +30,7 @@ gulp.task('validate', function() {
     .pipe(testPipeline.test());
 });
 
-gulp.task('build', ['clean', 'validate'] , function() {
+gulp.task('build', ['clean', 'validate'], function() {
 
   return gulp
     .src(config.cssFiles)
